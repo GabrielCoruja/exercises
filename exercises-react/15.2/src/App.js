fd093ad1a69adaa75f2d98160ff6399490a1e6fb
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-01-30 10:49:06 -0300

    adicionando mais exercicios

--- exercises-react/15.2/src/App.js
+++ exercises-react/15.2/src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+import { AboutMe, ContactForm, Projects } from './components';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const NavBar = () => (
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Sobre mim</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projetos</Link>
+        </li>
+        <li>
+          <Link to="/get-in-touch">Entre em contato</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+const Routes = () => (
+  <Switch>
+    <Route exact path="/">
+      <AboutMe />
+    </Route>
+
+    <Route path="/get-in-touch">
+      <ContactForm />
+    </Route>
+
+    <Route path="/projects">
+      <Projects />
+    </Route>
+
+    <Route>
+      <section>
+        <h1>Página não encontrada</h1>
+      </section>
+    </Route>
+  </Switch>
+);
+
+const App = () => (
+  <Router>
+    <NavBar />
+    <Routes />
+  </Router>
+);
 
 export default App;
--- exercises-react/15.2/src/App.test.js
+++ exercises-react/15.2/src/App.test.js
@@ -1,9 +1,61 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('react-router-dom', () => {
+  const originalModule = jest.requireActual('react-router-dom');
+
+  return {
+    ...originalModule,
+    BrowserRouter: ({ children }) => (<div>{children}</div>),
+  };
+});
+
+function renderWithRouter(
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+describe('routes', () => {
+  afterEach(cleanup);
+
+  test('navigating from home to projects', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const personalInfo = getByText(/Nome Completo/i);
+    expect(personalInfo).toBeInTheDocument();
+
+    fireEvent.click(getByText(/Projetos/i));
+
+    const project = getByText(/Projeto 1/i);
+    expect(project).toBeInTheDocument();
+  });
+
+  test('navigating from home to getInTouch', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const personalInfo = getByText(/Nome Completo/i);
+    expect(personalInfo).toBeInTheDocument();
+
+    fireEvent.click(getByText(/Entre em contato/i));
+
+    const getInTouch = getByText(/Mensagem de contato/i);
+    expect(getInTouch).toBeInTheDocument();
+  });
+
+  test('landing on a bad page shows error 404', () => {
+    const { getByText } = renderWithRouter(<App />, { route: '/some/bad/route' });
+
+    const pageNotFound = getByText(/Página não encontrada/i);
+    expect(pageNotFound).toBeInTheDocument();
+  });
 });
